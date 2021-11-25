436ce4d40155298348f552e45da96d0923130f17
Louis Auffret <louis.auffret76@gmail.com>
2021-11-25 10:36:43 +0100

    css + responsive terminé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,32 @@
+$(function(){
+    $('#start').click(function(){
+        var valPseudo = ($('#pseudo-bar').val()!='')? $('#pseudo-bar').val() : 'Player 1';
+        $('#playerName').html(valPseudo);
+        $('#menu').hide("slow");
+        $('#page-jeu').show("slow");
+
+    })
+    $('#menu-show').click(function(){
+        $('#page-jeu').hide("slow");
+        $('#menu').show("slow")
+    })
 
 
+    
+    else if(function(){
+        (CPU=='scisors'&& user=='paper')
+        ||(CPU=='paper'&& user=='rock')
+        ||(CPU=='rock'&& user=='scisors')
+        computerScore++;
+        changeColor('computer');
+    })
+    else{
+        userScore++;
+        changeColor('player');
+    }
+
+
+
+
+
+})
