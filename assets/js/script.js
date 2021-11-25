b218777ada2a7961b053961669f0a4c3d31d7202
Louis Auffret <louis.auffret76@gmail.com>
2021-11-25 19:25:00 +0100

    hover done + début de l'affichage POINT/MISS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,64 @@
 $(function(){
     let userChoice;
     let cpuChoice;
+    let choices = document.querySelectorAll('.playerChoice');
+
     let cptUser = 0;
     let cptCpu = 0;
     let winningGames = 3;
-    let choices = document.querySelectorAll('.playerChoice');
-        let audioOn = false; //variable qui permet d'éviter de rajouter la couche audio si on reclique sur 'START'.
-//SHOW HIDE des MENUS
+
+    let audioOn = false; //variable qui permet d'éviter de rajouter la couche audio si on reclique sur 'START'.
+//SHOW HIDE DES REGLES
+$('#close').click(function(){
+    $('#p-group').toggle();
+    $('#close').html(($('#close').html()=="Afficher les Règles")? 'RÈGLES' : 'Afficher les Règles');
+    });
+
+
+//SHOW HIDE des MENUS :
     $('#start').click(function(){
         var valPseudo = ($('#pseudo-bar').val()!='')? $('#pseudo-bar').val() : 'Player 1';
         $('#playerName').html(valPseudo);
         $('#menu').hide("slow");
         $('#page-jeu').show("slow");
 
-//MUSIQUE
-    if(audioOn===false) {
-        var song = new Audio();
-        song.src = 'Retrowave_Music.mp3';
-        song.play();
-        song.loop=true;
-        audioOn = true;
-    }
-
-
-//-----//
-
+    //MUSIQUE :
+        if(audioOn===false) {
+            var song = new Audio();
+            song.src = 'Retrowave_Music.mp3';
+            song.play();
+            song.loop=true;
+            audioOn = true;
+        }
     })
+
+//AFFRONTEMENT :
     $('#menu-show').click(function(){
         $('#page-jeu').hide("slow");
         $('#menu').show("slow")
     })
 
 
-
-
-
-    $('.playerChoice').draggable({ revert: "valid", revert: true });
+    $('.playerChoice').draggable({ revert: "valid", revert: true }); //rend l'élément déplaçable
 
 
     $('.playerChoice').on('dragstart', function (event) {
         userChoice = $(this).attr('id');
         $("#field-right, #field-left").html('');
-        
     });  
 
 
     $('#field-left').droppable({
         drop: function(event, ui) {
-            $(this).css('background', '#fcdc12').html(`<img class="image doubleSize" src="assets/img/${userChoice}.png">`);
+            $(this).css('background', 'linear-gradient(12deg, rgba(50,47,213,0.2) 5%, rgba(41,41,41,0.0) 50%, rgba(206,38,120,0.5) 100%)').html(`<img class="image doubleSize" src="assets/img/${userChoice}.png">`);
     
             fight();
         },
         over: function(event, ui) {
-            $(this).css('background', 'orange');
+            $(this).css('background', 'linear-gradient(12deg, rgba(50,47,213,0.6) 5%, rgba(41,41,41,0.2) 50%, rgba(206,38,120,0.8) 100%)');
         },
         out: function(event, ui) {
-            $(this).css('background', 'transparent');
+            $(this).css('background', 'linear-gradient(12deg, rgba(50,47,213,0.2) 5%, rgba(41,41,41,0) 50%, rgba(206,38,120,0.5) 100%)');
         },
         
     });
@@ -66,7 +69,7 @@ $(function(){
 
     function getCPUChoice() {
         cpuChoice = choices[Math.floor(Math.random() * choices.length)].id;
-        $("#field-right").css('background', '#fcdc12').html(`<img class="image doubleSize" src="assets/img/${cpuChoice}.png" >`);
+        $("#field-right").css('background', 'linear-gradient(12deg, rgba(50,47,213,0.2) 5%, rgba(41,41,41,0.0) 50%, rgba(206,38,120,0.5) 100%)').html(`<img class="image doubleSize" src="assets/img/${cpuChoice}.png" >`);
     }
 
     function fight() {
@@ -85,11 +88,32 @@ $(function(){
                 console.log(`Player wins`);
                 cptUser++;
                 $(`#circle-player-${cptUser}`).css('visibility', 'visible');
+                //ICI afficher le POINT!
+                // var afficher =  setTimeout(function(){document.getElementById('point').style.display = "block";},3000);
+                // setTimeout($('#point').hide(),5000);
+                // setTimeout(function(){$('#point').show();}, 3000);
+                function openWin() {
+                    var myWindow = $('#point');
+                    myWindow().show();
+                    setTimeout(function(){ myWindow.hide() }, 3000);
+                  }
+
             }
         else {
             console.log(`CPU wins`);
             cptCpu++;
             $(`#circle-cpu-${cptCpu}`).css('visibility', 'visible');
+                //ICI afficher le MISS!
+            // $('#miss').show();
+            // var afficher =  setTimeout(function(){document.getElementById('miss').style.display = "block";},3000);
+            // setTimeout($('#miss').hide(),5000);
+            // setTimeout(function(){$('#miss').show();}, 3000);
+            function openWin() {
+                var myWindow = $('#miss');
+                myWindow().show();
+                setTimeout(function(){ myWindow.hide() }, 3000);
+              }
+            
         }
 
         if((cptCpu==winningGames)||(cptUser==winningGames)){
