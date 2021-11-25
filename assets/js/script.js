6b5d1e0018ba0258581e4d87cde68f1ea88250b9
Thibault <ormieresthibault@hotmail.fr>
2021-11-25 15:32:14 +0100

    drag&drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 $(function(){
+    let userChoice;
+    let cpuChoice;
+    let choices = document.querySelectorAll('.playerChoice');
         let audioOn = false; //variable qui permet d'éviter de rajouter la couche audio si on reclique sur 'START'.
 //SHOW HIDE des MENUS
     $('#start').click(function(){
@@ -26,19 +29,61 @@ $(function(){
     })
 
 
-    
-    // else if(function(){
-    //     (CPU=='scisors'&& user=='paper')
-    //     ||(CPU=='paper'&& user=='rock')
-    //     ||(CPU=='rock'&& user=='scisors')
-    //     computerScore++;
-    //     changeColor('computer');
-    // })
-    // else{
-    //     userScore++;
-    //     changeColor('player');
-    // }
 
+
+
+    $('.playerChoice').draggable({ revert: "invalid" });
+
+
+    $('.playerChoice').on('dragstart', function (event) {
+        userChoice = $(this).attr('id');
+        $("#field-right").html('');
+    });  
+
+
+    $('#field-left').droppable({
+        drop: function(event, ui) {
+            $(this).css('background', '#fcdc12');
+            fight();
+        },
+        over: function(event, ui) {
+            $(this).css('background', 'orange');
+        },
+        out: function(event, ui) {
+            $(this).css('background', 'transparent');
+        },
+        
+    });
+
+
+
+
+
+    function getCPUChoice() {
+        cpuChoice = choices[Math.floor(Math.random() * choices.length)].id;
+        $("#field-right").css('background', '#fcdc12').html(`<img class="image" src="assets/img/${cpuChoice}.png">`);
+    }
+
+    function fight() {
+        getCPUChoice();
+        console.log(`userChoice = ${userChoice}`);
+        console.log(`cpuChoice = ${cpuChoice}`);
+
+        if (userChoice == cpuChoice) {
+            console.log(`Egalité`);
+        }
+        else if (
+                (userChoice == 'rock' && cpuChoice == 'scissors')
+                || (userChoice == 'scissors' && cpuChoice == 'paper')
+                || (userChoice == 'paper' && cpuChoice == 'rock')
+            ){
+                console.log(`Player wins`);
+            }
+        else {
+            console.log(`CPU wins`);
+        }
+
+    }
 
 
 
