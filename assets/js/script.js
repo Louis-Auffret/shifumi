e6459896de4f40dad2a2c0803366ced8389445b0
Louis Auffret <louis.auffret76@gmail.com>
2021-11-25 14:42:57 +0100

    images + musique

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,23 @@
 $(function(){
+        let audioOn = false; //variable qui permet d'éviter de rajouter la couche audio si on reclique sur 'START'.
+//SHOW HIDE des MENUS
     $('#start').click(function(){
         var valPseudo = ($('#pseudo-bar').val()!='')? $('#pseudo-bar').val() : 'Player 1';
         $('#playerName').html(valPseudo);
         $('#menu').hide("slow");
         $('#page-jeu').show("slow");
+
+//MUSIQUE
+    if(audioOn===false) {
+        var song = new Audio();
+        song.src = 'Retrowave_Music.mp3';
+        song.play();
+        song.loop=true;
+        audioOn = true;
+    }
+
+
+//-----//
 
     })
     $('#menu-show').click(function(){
@@ -12,18 +26,20 @@ $(function(){
     })
 
 
-    
-    else if(function(){
-        (CPU=='scisors'&& user=='paper')
-        ||(CPU=='paper'&& user=='rock')
-        ||(CPU=='rock'&& user=='scisors')
-        computerScore++;
-        changeColor('computer');
-    })
-    else{
-        userScore++;
-        changeColor('player');
-    }
+
+    // $('.play-element').draggable({revert:"valid",revertDuration:600})
+    // $('#field-left').droppable({snap = true})
+    // else if(function(){
+    //     (CPU=='scisors'&& user=='paper')
+    //     ||(CPU=='paper'&& user=='rock')
+    //     ||(CPU=='rock'&& user=='scisors')
+    //     computerScore++;
+    //     changeColor('computer');
+    // })
+    // else {
+    //     userScore++;
+    //     changeColor('player');
+    // }
 
 
 
